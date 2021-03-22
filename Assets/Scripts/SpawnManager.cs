780dd4fddd0f90a6ae4667060a29b04f85ba2c21
Serge Satansson <thornonthehead@gmail.com>
2021-03-22 11:12:13 +0300

    SpawnEnemyScript has been changed

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,48 +8,44 @@ public class SpawnManager : MonoBehaviour
     [SerializeField] float maxTime;
     [SerializeField] int maxX = 960;
     [SerializeField] int maxY = 540;
-    [SerializeField] int maxEnemiesCount;
+    [SerializeField] int maxEnemiesAmount;
     [SerializeField] GameObject[] enemyPrefabs;
 
-    int enemiesCount = 0;
+    int enemiesAmount = 0;
 
     Vector3 spawnPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine("SpawnEnemyRoutine");
-    }
-
-    IEnumerator SpawnEnemyRoutine()
-    {
-        float randTime = Random.Range(minTime, maxTime);
-        float spawnRate = 1 + randTime / Time.time;
-
-        yield return new WaitForSeconds(.5f);
-
-        SpawnEnemy();
-        Debug.Log("Enemy was called");
+        Invoke("SpawnEnemy", 1);
     }
 
     void SpawnEnemy()
     {
-        int randBorder = Random.Range(0, 3);
-        int randXRange = Random.Range(-maxX, maxX);
-        int randYRange = Random.Range(-maxY, maxY);
-        int randEnemy = Random.Range(0, enemyPrefabs.Length);
+        int randomBorder = Random.Range(0, 3);
+        int randomXRange = Random.Range(-maxX, maxX);
+        int randomYRange = Random.Range(-maxY, maxY);
+        int randomEnemy = Random.Range(0, enemyPrefabs.Length);
 
-        if (randBorder == 0)
-            spawnPosition = new Vector3(randXRange, maxY, 0);
-        else if (randBorder == 1)
-            spawnPosition = new Vector3(randXRange, -maxY, 0);
-        else if (randBorder == 2)
-            spawnPosition = new Vector3(maxX, randYRange, 0);
-        else if (randBorder == 3)
-            spawnPosition = new Vector3(-maxX, randYRange, 0);
+        float randTime = Random.Range(minTime, maxTime);
+        float spawnRate = 1 + randTime / Time.time;
 
-        Instantiate(enemyPrefabs[randEnemy], spawnPosition, enemyPrefabs[randEnemy].transform.rotation);
-        enemiesCount++;
-        Debug.Log("Enemy was spawned");
-    }    
+        if (randomBorder == 0)
+            spawnPosition = new Vector3(randomXRange, maxY, 0);
+        else if (randomBorder == 1)
+            spawnPosition = new Vector3(randomXRange, -maxY, 0);
+        else if (randomBorder == 2)
+            spawnPosition = new Vector3(maxX, randomYRange, 0);
+        else if (randomBorder == 3)
+            spawnPosition = new Vector3(-maxX, randomYRange, 0);
+
+        if (enemiesAmount < maxEnemiesAmount)
+        {
+            Instantiate(enemyPrefabs[randomEnemy], spawnPosition, enemyPrefabs[randomEnemy].transform.rotation);
+            enemiesAmount++;
+        }
+
+        Invoke("SpawnEnemy", spawnRate);
+    }
 }
